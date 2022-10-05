5c118d5e7874ffbec97e5d0117cbd9e6572d92b1
Aimar Karrik <71828910+AimarKarrik@users.noreply.github.com>
2022-10-05 14:12:28 +0300

    completed CRUD for Users

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # todo-backend
 
+```bash
+        .
+      ":"
+    ___:____     |"\/"|
+  ,'        `.    \  /
+  |  O        \___/  |
+~^~^~^~^~^~^~^~^~^~^~^~^~
+```
+
 ## How to run
 
 ### Run the server
@@ -7,3 +16,12 @@
 ```bash
     npm start
 ```
+
+## Todo
+
+1. CRUD API endpoints User and Task Models/Entities. Object, Array, Class
+2. After every change save data to a file.
+  - Read data from file on startup.
+  - Read data from json file for queries
+3. Login and Register with sessions.
+4. Replace the file system with a database.
--- index.js
+++ index.js
@@ -1,35 +1,97 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
+app.use(express.json())
 const port = 3000
 
-const users = [];
-const tasks = [];
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 // crud api endpoints for users
-app.get('/users', (req, res) => {
-  res.send(users)
+app.get('/api/users', (req, res) => {
+  const users = JSON.parse(fs.readFileSync('./data/users.json'))
+  publicUsers = users.map(function(i) {
+    return {
+      id: i.id,
+      name: i.name
+    }
+  })
+  res.send(publicUsers)
 })
 
-app.post('/users', (req, res) => {
-  users.push(req.body)
-  res.send('User created')
+app.post('/api/users', (req, res) => {
+  const newUser = req.body;
+  newUser.id = Math.floor(Math.random() * 1000000);
+
+  fs.readFile('./data/users.json', (err, data) => {
+    if(err) {
+      console.log(err);
+    }
+    else{
+      var users = JSON.parse(data);
+      users.push(newUser);
+      fs.writeFile('./data/users.json', JSON.stringify(users), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      })
+    }
+  })
+  res.send(newUser);
 })
 
-app.put('/users/:id', (req, res) => {
-  users[req.params.id] = req.body
-  res.send('User updated')
+app.put('/api/users/:id', (req, res) => {
+  updatedUserId = req.params["id"];
+  updatedUser = req.body;
+  updatedUser.id = parseInt(updatedUserId);
+
+  fs.readFile('./data/users.json', (err, data) => {
+    if(err) {
+      console.log(err);
+    }
+    else{
+      var users = JSON.parse(data);
+
+      updatedUserIndex = users.findIndex(user => user.id == updatedUserId);
+      users[updatedUserIndex] = updatedUser;
+
+      fs.writeFile('./data/users.json', JSON.stringify(users), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      })
+    }
+  })
+  res.send('User: ' + updatedUserId + ' has been updated');
 })
 
-app.delete('/users/:id', (req, res) => {
-  users.splice(req.params.id, 1)
-  res.send('User deleted')
+app.delete('/api/users/:id', (req, res) => {
+  deletedUserId = req.params["id"];
+
+  fs.readFile('./data/users.json', (err, data) => {
+    if(err) {
+      console.log(err);
+    }
+    else{
+      var users = JSON.parse(data);
+
+      deletedUserIndex = users.findIndex(user => user.id == deletedUserId);
+      users.splice(deletedUserIndex, 1);
+
+      fs.writeFile('./data/users.json', JSON.stringify(users), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      })
+    }
+  })
+  res.send('User: ' + deletedUserId + ' has been deleted');
 })
 
 // crud api endpoints for tasks
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
